af3601307f3f57b10d77d2fc41f6fed80eedff81
PavelEnodd <pavelenodd@gmail.com>
2024-04-06 20:21:27 +0300

    улучшил логику , перевёл часть синтаксиса в engine_data.h

--- Src/Engine/InputsOutputs/input_output.cpp
+++ Src/Engine/InputsOutputs/input_output.cpp
@@ -15,8 +15,7 @@ class Outputs {
  private:
   EngineSettings engine_settings_;  // настройки рендера вьюпорта
 
-  void PrintToLogWindowSettongs(sf::ContextSettings L_window_settings,
-                                bool is_vertical_sync_enabled_) {
+  void PrintToLogWindowSettongs(const EngineSettings& L_engine_settings) {
     // ^вывод консоьной информации о рендере и настройках вьюпорта
 
     // зона подключения модификаторов вывода
@@ -27,28 +26,28 @@ class Outputs {
               << "\033[4m"
               << "depth bits"
               << "\033[0m"
-              << "\t\t" << engine_settings_.depth_bits << "\n";
+              << "\t\t" << L_engine_settings.depth_bits << "\n";
     std::cerr << "\t"
               << "\033[4m"
               << "stencil bits"
               << "\033[0m"
-              << "\t\t" << engine_settings_.stencil_bits << "\n";
+              << "\t\t" << L_engine_settings.stencil_bits << "\n";
     std::cerr << "\t"
               << "\033[4m"
               << "antialiasing level"
               << "\033[0m"
-              << "\t" << engine_settings_.antialiasing_level << "\n";
+              << "\t" << L_engine_settings.antialiasing_level << "\n";
     std::cerr << "\t"
               << "\033[4m"
               << "vertical sync"
               << "\033[0m"
-              << "\t\t" << engine_settings_.is_vertical_sync_enabled << "\n";
+              << "\t\t" << L_engine_settings.is_vertical_sync_enabled << "\n";
     std::cerr << "\t"
               << "\033[4m"
               << "OpenGL version"
               << "\033[0m"
-              << "\t\t" << engine_settings_.major_version << "."
-              << engine_settings_.minor_version << "\n";
+              << "\t\t" << L_engine_settings.major_version << "."
+              << L_engine_settings.minor_version << "\n";
     // зона отключения всех модификаторов
     std::cerr << std::noboolalpha;
 
@@ -60,21 +59,14 @@ class Outputs {
   Outputs() {}
   ~Outputs() {}
   sf::Window* CreateViewport(const EngineSettings& L_engine_settings) {
-    sf::ContextSettings L_const_settings;
-    L_const_settings.antialiasingLevel = L_engine_settings.antialiasing_level;
-    L_const_settings.attributeFlags = L_engine_settings.attribute_flags;
-    L_const_settings.depthBits = L_engine_settings.depth_bits;
-    L_const_settings.stencilBits = L_const_settings.stencilBits;
+    PrintToLogWindowSettongs(L_engine_settings);
 
-    L_const_settings.majorVersion = L_engine_settings.major_version;
-    L_const_settings.minorVersion = L_engine_settings.minor_version;
-
-    L_const_settings.sRgbCapable = L_engine_settings.sRGB_capable;
-
-    sf::Window* window = new sf::Window(
-        sf::VideoMode(L_engine_settings.vieport_size.height,
-                      L_engine_settings.vieport_size.width),
-        L_engine_settings.vieport_name, sf::Style::Default, L_const_settings);
+    sf::Window* window =
+        new sf::Window(sf::VideoMode(L_engine_settings.vieport_size.height,
+                                     L_engine_settings.vieport_size.width),
+                       L_engine_settings.vieport_name,
+                       sf::Style::Default,  //
+                       L_engine_settings.Get_ContextSettings());
     return window;
   }
 };
--- Src/Engine/engine_data.h
+++ Src/Engine/engine_data.h
@@ -1,9 +1,11 @@
 #pragma once
+#include <SFML/OpenGL.hpp>
 #include <string>
 struct VieportSize {
   int height;
   int width;
 };
+
 struct EngineSettings {
   VieportSize vieport_size;  //<
   int depth_bits;            //< Bits of the depth buffer
@@ -15,4 +17,16 @@ struct EngineSettings {
   bool sRGB_capable;         //< Whether the context framebuffer is sRGB capable
   bool is_vertical_sync_enabled = false;  //<
   std::string vieport_name = "Viewport";  //<
+
+  sf::ContextSettings Get_ContextSettings() const {
+    sf::ContextSettings L_const_settings;
+    L_const_settings.antialiasingLevel = antialiasing_level;
+    L_const_settings.attributeFlags = attribute_flags;
+    L_const_settings.depthBits = depth_bits;
+    L_const_settings.stencilBits = stencil_bits;
+    L_const_settings.majorVersion = major_version;
+    L_const_settings.minorVersion = minor_version;
+    L_const_settings.sRgbCapable = sRGB_capable;
+    return L_const_settings;
+  }
 };
