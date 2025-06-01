f941dfbc2ca5073e4d94fa80463a8bc72336508c
Pavel Bobrovskiy <Pavelenodd@gmail.com>
2025-06-01 13:35:21 +0300

    # chore: Оптимизация сборки и документации проекта
    ### #Изменение в файле CMakeLists.txt
    - Перемещена команда поиска библиотеки nlohmann_json вверх списка зависимостей
    - Уточнена документация для добавления пакета Vulkan в проект
    
    ### #Удалённый файл Doc/info
    - Полностью удалён файл info с устаревшей документацией классов engine_error и engine_logging
    
    ### #Изменение в файле Doc/manager_info.md
    - Переработан порядок пунктов и оформление раздела менеджера компонентов
    - Добавлен новый элемент `manager_settings` для обработки настроек движка и игры
    
    ### #Изменение в файле Src/Settings/keys.jsonc
    - Добавлены комментарии для клавиш выхода из игры
    - Создан новый раздел для команд мыши с привязкой кликов к соответствующим действиям
    
    Оптимизирован процесс сборки и обновлена документация для повышения удобства поддержки проекта.

---
+++ Src/Managers/manager_settings.h
@@ -0,0 +1,55 @@
+#pragma once
+#include <fstream>
+#include <map>
+#include <nlohmann/json.hpp>
+#include <string>
+
+#include "manager_base.h"
+
+namespace EDD {
+enum SettingsType {
+  WINDOW_SETTINGS = 0,
+  AUDIO_SETTINGS = 1,
+  GRAPHICS_SETTINGS = 2
+};
+
+class ManagerSettings : public Managers::Base {
+ private:
+  nlohmann::json settings_;
+  std::map<std::string, SettingsType> settings_map_;
+
+ private:
+  /**
+   * @brief Проверяет, можно ли открыть файл по указанному пути
+   * @param file_path Путь к файлу
+   */
+  bool IsOpenFile(const std::string& file_path) {
+    std::ifstream L_file(file_path);
+    if (L_file.fail()) {
+      return false;
+    }
+    L_file.open(file_path);
+    if (!L_file.is_open()) {
+      return false;
+    }
+    return true;
+  }
+  /**
+   * @brief Закрывает файл, если он открыт
+   * @param file_path Путь к файлу для закрытия
+   */
+  void CloseFile(const std::string& file_path) {
+    std::ifstream L_file(file_path);
+    if (L_file.is_open()) {
+      L_file.close();
+    }
+  }
+
+ public:
+  ManagerSettings() {}
+  ~ManagerSettings() {}
+  void LoadSettings(const std::string& file_path) {}
+  void SaveSettings(const std::string& file_path) {}
+};
+
+}  // namespace EDD
